3ae929f773d7a71619d8dd84aba5a953eab24011
Erica Sun <ericasun@seas.upenn.edu>
2020-07-24 02:46:19 +0000

    Use ENV for admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_AUTH_USERNAME"], password: ENV["ADMIN_AUTH_PASSWORD"]
+
   def show
     @productCount = Product.count 
     @categoryCount = Category.count

a4201a762cd36ca7a8a4d63e091221c9c45afc4b
Erica Sun <ericasun@seas.upenn.edu>
2020-07-27 20:39:50 +0000

    Fix expect statements based on code review with mentor

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,35 +5,38 @@ RSpec.describe Product, type: :model do
     
     before(:each) do
       @category = Category.create(name: "Office")
-      expect(@category).to be_present
-      expect(@category.name).to eq("Office")
     end
 
     it 'should save' do
-      @product = Product.create(name: "Desk", price: 20000, quantity: 40, category_id: 20)
-      expect(@product).to be_present
+      @product = @category.products.new(name: "Desk", price: 20000, quantity: 40)
+      expect(@product).to be_valid
+      puts @product.errors.full_messages
     end
 
     it 'should have a name' do
-      @product = Product.create(name: nil, price: 10000, quantity: 35, category_id: 20)
-      expect(@product.name).to be_present
+      @product = @category.products.new(name: nil, price: 10000, quantity: 35)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'should have a price' do
-      @product = Product.create(name: "Shelf", price: nil, quantity: 35, category_id: 20)
-      expect(@product.price).to be_present
+      @product = @category.products.new(name: "Shelf", price: nil, quantity: 35)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
     end
 
     it 'should have a quantity' do
-      @product = Product.create(name: "Shelf", price: 10000, quantity: nil, category_id: 20)
-      expect(@product.quantity).to be_present
+      @product = @category.products.new(name: "Shelf", price: 10000, quantity: nil)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'should have a category' do
-      @product = Product.create(name: "Shelf", price: 10000, quantity: 35, category_id: nil)
-      expect(@product.category_id).to be_present
+      @product = Product.new(name: "Shelf", price: 10000, quantity: 35, category_id: nil)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+      
     end
-
 
   end
 end

3123922c45fc38b4444e8a998294026274dcc10b
Erica Sun <ericasun@seas.upenn.edu>
2020-07-28 04:11:54 +0000

    Add failing test cases and fix code to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates_uniqueness_of :email, case_sensitive: false
   validates :password, length: { minimum: 2 }
+  before_save { email.downcase! } 
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,14 +41,56 @@ RSpec.describe User, type: :model do
     it 'should have unique email field' do
       @user1 = User.create(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "hi", password_confirmation: "hi")
       @user2 = User.new(first_name: "John", last_name: "Smith", email: "ericaSUN@email.com", password: "hello", password_confirmation: "hello")
-      byebug
       expect(@user2).to_not be_valid
     end
 
     it 'should have password minimum length' do
       @user = User.new(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "h", password_confirmation: "h")
-      expect(@user).to_not be_valid
+      expect(@user).not_to be_valid
     end
 
   end
+
+  describe '.authenticate_with_credentials' do
+
+    it 'should log user in if credentials are correct' do
+      @user = User.create(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "hello", password_confirmation: "hello")
+      expect(User.authenticate_with_credentials("ericasun@email.com", "hello")).to eq(@user)
+    end
+
+    it 'should not log user in if email is missing' do
+      @user = User.create(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "hello", password_confirmation: "hello")
+      expect(User.authenticate_with_credentials("", "hello")).to be_nil
+    end
+
+    it 'should not log user in if password is missing' do
+      @user = User.create(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "hello", password_confirmation: "hello")
+      expect(User.authenticate_with_credentials("ericasun@email.com", "")).to be_nil
+    end
+
+    it 'should log user in regardless of email case' do
+      @user = User.create(first_name: "Erica", last_name: "Sun", email: "eRicasun@email.com", password: "hello", password_confirmation: "hello")
+      expect(User.authenticate_with_credentials("eRiCaSun@email.COm", "hello")).to eq(@user)
+    end 
+
+    it 'should not log user in if password case is different' do
+      @user = User.create(first_name: "Erica", last_name: "Sun", email: "ericasun@email.com", password: "hello", password_confirmation: "hello")
+      expect(User.authenticate_with_credentials("ericasun@email.com", "heLlo")).to be_nil
+    end
+
+    it 'should log user in regardless of email spaces before' do
+    end 
+
+    it 'should log user in regardless of email spaces after' do
+    end 
+
+    it 'should log user in regardless of email spaces before and after' do
+    end 
+
+
+
+
+
+  end
+
 end
